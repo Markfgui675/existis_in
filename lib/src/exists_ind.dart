d16a38f2be3c41e59a3674c30f254d97668ecc5a
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2023-05-03 18:02:13 -0300

    Aplicação de lógica do algoritmo.

--- lib/src/exists_ind.dart
+++ lib/src/exists_ind.dart
@@ -1,5 +1,15 @@
 import 'dart:math';
 
-existsIn(dynamic element, List list){
-
+existsIn(dynamic element, List<dynamic> list){
+  int i = 0;
+  for (int c = 0; i < list.length; i++){
+    if (i != list[c]){
+      i++;
+    }
+    if (i == ((list.length) - 1)){
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
